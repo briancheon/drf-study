e574133a6410f01aa1eee797b8352288f4653513
천우영 <brian1000jj@gmail.com>
2025-08-29 17:37:54 +0900

    feat: Implement Application CRUD API

--- apps/core/serializers/application.py
+++ apps/core/serializers/application.py
@@ -1,41 +1,65 @@
+from django.db import transaction
 from rest_framework import serializers
 
-from apps.core.models import Application, ApplicationField, Field
-from apps.core.serializers.application_field import ApplicationFieldSerializer
+from apps.core.models import Application, ApplicationField, Field, Recruitment
+from apps.core.serializers.field import FieldSerializer
 from apps.core.serializers.user import UserSerializer
 
-# 생성/수정용
+
 class ApplicationSerializer(serializers.ModelSerializer):
-    fields = serializers.ListField(
-        child=serializers.IntegerField()
-    )
+    fields = serializers.ListField(child=serializers.IntegerField(), write_only=True)
+
     class Meta:
         model = Application
-        fields = ['recruitment', 'content', 'fields']
-
-    def validate_fields(self, value):
-        for field in value:
-            if not Field.objects.get(id=field):
-                raise serializers.ValidationError(f"Field with id {field} does not exist.")
-        return value
+        fields = ["id", "recruitment", "content", "fields"]
+        read_only_fields = ["id"]
 
     def create(self, validated_data):
-        fields_data = validated_data.pop('fields', None)
-        application = Application.objects.create(**validated_data)
+        fields_data = validated_data.pop("fields", [])
+        applicant = self.context["request"].user
+        recruitment_id = validated_data.get("recruitment").id
 
-        for field_id in fields_data:
-            ApplicationField.objects.create(
-                application=application,
-                field_id=field_id
-            )
+        if Application.objects.filter(applicant=applicant, recruitment_id=recruitment_id).exists():
+            raise serializers.ValidationError("You have already applied for this recruitment.")
 
+        with transaction.atomic():
+            application = Application.objects.create(applicant=applicant, **validated_data)
+            for field_id in fields_data:
+                field = Field.objects.get(id=field_id)
+                ApplicationField.objects.create(application=application, field=field)
         return application
 
-# 조회용
+    def update(self, instance, validated_data):
+        fields_data = validated_data.pop("fields", None)
+
+        with transaction.atomic():
+            instance.content = validated_data.get("content", instance.content)
+            instance.save()
+
+            if fields_data is not None:
+                instance.application_field_set.all().delete()
+                for field_id in fields_data:
+                    field = Field.objects.get(id=field_id)
+                    ApplicationField.objects.create(application=instance, field=field)
+
+        return instance
+
+
 class ApplicationDetailSerializer(serializers.ModelSerializer):
     applicant = UserSerializer(read_only=True)
-    fields = ApplicationFieldSerializer(many=True, read_only=True, source="application_field_set")
+    fields = FieldSerializer(many=True, read_only=True, source="application_field_set")
 
     class Meta:
         model = Application
-        fields = ['id', 'applicant', 'recruitment', 'content', 'fields']
+        fields = [
+            "id",
+            "applicant",
+            "recruitment",
+            "content",
+            "fields",
+        ]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["fields"] = [field["name"] for field in representation["fields"]]
+        return representation
--- apps/core/views/application.py
+++ apps/core/views/application.py
@@ -1,49 +1,30 @@
+from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 from apps.core.models import Application
 from apps.core.serializers.application import ApplicationSerializer, ApplicationDetailSerializer
 
 
-class ApplicationListView(APIView):
-    def get(self, request):
-        applications = Application.objects.all().prefetch_related("application_field_set", "applicant")
-        serializer = ApplicationDetailSerializer(applications, many=True)
-        return Response(serializer.data, status=200)
+class ApplicationViewSet(viewsets.ModelViewSet):
+    queryset = Application.objects.all()
+    permission_classes = [IsAuthenticated]
 
-    def post(self, request):
-        user = request.user
-        serializer = ApplicationSerializer(data=request.data)
-        if serializer.is_valid():
-            application = serializer.save(applicant=user)
-            return Response(ApplicationDetailSerializer(application).data, status=201)
-        return Response(serializer.errors, status=400)
+    def get_serializer_class(self):
+        if self.action in ["retrieve", "recruitments"]:
+            return ApplicationDetailSerializer
+        return ApplicationSerializer
 
-class ApplicationRecruitmentDetailView(APIView):
-    def get(self, request, recruitment_id):
-        applications = Application.objects.filter(recruitment_id=recruitment_id).prefetch_related("application_field_set")
-        serializer = ApplicationDetailSerializer(applications, many=True)
-        return Response(serializer.data, status=200)
+    def get_queryset(self):
+        queryset = super().get_queryset().select_related("applicant").prefetch_related("application_field_set__field")
+        return queryset
 
-
-class ApplicationDetailView(APIView):
-    def get_object(self, pk):
-        return Application.objects.get(pk=pk)
-
-    def get(self, request, pk):
-        application = self.get_object(pk)
-        serializer = ApplicationDetailSerializer(application)
+    @action(detail=False, methods=["get"], url_path="recruitments/(?P<recruitment_id>[^/.]+)")
+    def recruitments(self, request, recruitment_id=None):
+        """
+        모집글에 대한 신청글 조회
+        """
+        applications = self.get_queryset().filter(recruitment_id=recruitment_id)
+        serializer = self.get_serializer(applications, many=True)
         return Response(serializer.data)
-
-    def put(self, request, pk):
-        application = self.get_object(pk)
-        serializer = ApplicationSerializer(application, data=request.data)
-        if serializer.is_valid():
-            application = serializer.save()
-            return Response(ApplicationDetailSerializer(application).data, status=200)
-        return Response(serializer.errors, status=400)
-
-    def delete(self, request, pk):
-        application = self.get_object(pk)
-        application.delete()
-        return Response(status=204)
